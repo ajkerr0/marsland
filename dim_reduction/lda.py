99d60b0e69a1b5f19961021caf42dc56a5ffc2ad
Alex Kerr <ajkerr0@gmail.com>
2016-10-24 00:57:19 -0500

    Flesh out lda function, add iris data

--- dim_reduction/lda.py
+++ dim_reduction/lda.py
@@ -4,17 +4,20 @@ Linear discriminant analysis
 """
 
 import numpy as np
+import scipy.linalg
+import matplotlib.pyplot as plt
 
-def lda(data, labels):
+def lda(data, labels, redDim):
     
     data = np.array(data)
+#    data -= data.mean(axis=0)
     
     #covariance matrix
     #rows corresponds to variable, columns to data points for numpy function
-    C = np.cov(np.transpose(data))
+    cov = np.cov(np.transpose(data))
     
     #initialize within-class scatter
-    sw = np.zeros((data.shape[1], data.shape[1]))    
+    sw = np.zeros((data.shape[1], data.shape[1]))
     #get the unique labels, which are the classes
     classes = np.unique(labels)
     for class_ in classes:
@@ -24,6 +27,30 @@ def lda(data, labels):
         #contribute to within-class scatter
         #probability of being in that class times class covariance
         sw += (d.shape[0]/data.shape[0])*np.cov(np.transpose(d))
+        
+    sb = cov - sw
+    
+    val, vec = scipy.linalg.eig(sw, sb)
+    indices = np.argsort(val)
+    indices = indices[::-1]
+    vec = vec[:,indices]
+    val = val[indices]
+    w = vec[:,:redDim]
+    newData = np.dot(data,w)
+    return newData,w
+    
+iris = np.loadtxt("./iris.data", delimiter=',')
+data, labels = iris[:,:-1], iris[:,-1].astype(int)
+
+#print(data, labels)
+    
+#data = np.array([[0.1,0.1],[0.2,0.2],[0.3,0.3],[0.35,0.3],[0.4,0.4],[0.6,0.4],[0.7,0.45],[0.75,0.4],[0.8,0.35]])
+#labels = np.array([0,0,0,0,0,1,1,1,1])
+newData,w = lda(data,labels,3)
+print(w)
+plt.plot(data[:,0],data[:,1],'o',newData[:,0],newData[:,0],'.')
+plt.show()
+
         
     
         
